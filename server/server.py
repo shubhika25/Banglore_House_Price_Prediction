111a105a4a29ddcc403a37bdae2be8d8ede4f3d9
Shubhika <Shubhikasingh253@gmail.com>
2025-07-28 17:18:09 +0530

    change app.run

--- server/server.py
+++ server/server.py
@@ -36,4 +36,4 @@ if __name__ == '__main__':
         import os
         port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
         app.run(host='0.0.0.0', port=port)
-        app.run(debug=True)
+  
